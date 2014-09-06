d314aea93fac8d052d57a70ae71a75ab324870c2
hpsoar <hpsoar@gmail.com>
2014-09-06 20:49:57 +0800

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by HuangPeng on 9/6/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by HuangPeng on 9/6/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
